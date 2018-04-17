fdf5a715ae68b938ed723f522790f48932421748
Jack Campbell <jcampbell@uaudio.com>
2018-04-17 15:04:13 -0600

    Initial Commit

---
+++ threading_examples/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  threading_examples
+//
+//  Created by Jack Campbell on 4/17/18.
+//  Copyright © 2018 Jack Campbell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
